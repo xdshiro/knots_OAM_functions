f4110090926905e026da4dda0b3442e5858343c1
xDShiro_Home <cmex-d@yandex.ru>
2022-06-16 20:19:07 -0400

    All the plots finished. added Hopf vs Trefoil comparison

--- Leetcode.py
+++ Leetcode.py
@@ -2,13 +2,19 @@ def twoSum(nums, target):
     for element in enumerate(nums):
         if target - element[1] in nums[element[0] + 1:]:
             return [element[0], element[0] + 1 + nums[element[0] + 1:].index(target - element[1])]
-    return 0
+    return (0)
 
 
-print(twoSum(
-    [3, 2, 4],
-    6)
-)
+print(f'Twinkle, twinkle, little star,\n\t How I wonder '
+      f'what you are!\n\t\t Up above the world so high,\n\t\t Like '
+      f'a diamond in the sky.\n Twinkle, twinkle, '
+      f'little star,\n\t How I wonder what you are')
+print(print(f'Twinkle, twinkle, little star,\n\t How I wonder '
+            f'what you are!\n\t\t Up above the world so high,\n\t\t Like '
+            f'a diamond in the sky.\n Twinkle, twinkle, '
+            f'little star,\n\t How I wonder what you are'))
+
+
 def permute2(seq):
     if not seq:
         yield seq
@@ -21,3 +27,7 @@ def permute2(seq):
 
 for x in permute2('123'):
     print(x)
+
+a = 'Zina'
+b = a[len(a) - 1: -1]
+print(b)
